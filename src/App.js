8a53a9045d80e5a95f44482fd1abf588a3ebf5f3
Shitiz Khanna <shitizmvn@gmail.com>
2019-06-14 21:50:34 +0530

    Implementing a component and a little bit styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import Person from "./Person/Person";
+class App extends React.Component {
+  state = {
+    persons: [
+      {
+        name: "Max",
+        age: 26
+      },
+      {
+        name: "Stephanie",
+        age: 28
+      },
+      {
+        name: "Sony",
+        age: 29
+      }
+    ]
+  };
+  render() {
+    return (
+      <div className="App-link">
+        <header className="App-header">
+          <p>
+            <img
+              src="https://cdn4.iconfinder.com/data/icons/logos-3/426/react_js-512.png"
+              className="App-logo"
+              alt="Loading"
+            />
+          </p>
+
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <button>Go</button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          />
+          <Person />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "../App.css";
+const person = () => {
+  return <div> It's {new Date().toLocaleTimeString()}</div>;
+};
+export default person;
